307dccb050316b0a1a58ed8da444867167577e4e
kirtdave <kirtdave4@gmail.com>
2026-02-19 10:10:53 +0800

    middleware bot detection

--- middleware.js
+++ middleware.js
@@ -1,19 +1,12 @@
-import { NextResponse } from 'next/server'
+import { NextResponse } from "next/server";
 
 export function middleware(request) {
-  const ua = request.headers.get('user-agent') || ''
-  if (ua.includes('facebookexternalhit') || ua.includes('Facebot')) {
-    return NextResponse.rewrite(new URL('/api/og', request.url))
+  const ua = request.headers.get("user-agent") || "";
+  if (ua.includes("facebookexternalhit") || ua.includes("Facebot")) {
+    return NextResponse.rewrite(new URL("/api/og", request.url));
   }
 }
 
 export const config = {
-  matcher: '/',
-}
-```
-
-Then push:
-```
-git add .
-git commit -m "middleware bot detection"
-git push
+  matcher: "/",
+};
